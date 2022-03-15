a17c148e9a69ab1daaa57bc1e15ec1dc232925cf
Erik Jenson <erik.a.jenson@gmail.com>
2022-03-14 23:56:36 -0400

    game

--- projects/main.js
+++
@@ -1,7 +0,0 @@
-const layout = require('./layout');
-const html = require('html-template-tag');
-
-module.exports = () =>
-  layout(html`<div class="img-container">
-    <img src="/photo.jpg" />
-  </div>`);
